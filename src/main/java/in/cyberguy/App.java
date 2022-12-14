9ab27d874aaa41454e2df33cb132012abfdf7fd6
CyberGuy Training <cyberguytraining@gmail.com>
2022-12-14 17:50:59 +0530

    msg

---
+++ src/main/java/in/cyberguy/App.java
@@ -0,0 +1,13 @@
+package in.cyberguy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
